6c134748a8e2a5e4f57cdcb6c6652d0c3eae20cb
Bernard Polidario <bernard@weeklyhow.com>
2023-05-31 19:40:29 +0200

    Using callbacks for the session

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,33 +10,35 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session({ session }) {
-        // Make sure to always update the session
-        const user = await User.findOne({ email: session.user.email });
-        session.user.id = user._id.toString();
-
-        return session;
-    },
-    async signIn({ profile }) {
-        try {
-            await dbConnection();
-
-            const userExists = await User.findOne({
-                email: profile.email
-            });
-
-            if( !userExists ) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture,
+    callbacks: {
+        async session({ session }) {
+            // Make sure to always update the session
+            const user = await User.findOne({ email: session.user.email });
+            session.user.id = user._id.toString();
+    
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                await dbConnection();
+    
+                const userExists = await User.findOne({
+                    email: profile.email
                 });
+    
+                if( !userExists ) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture,
+                    });
+                }
+    
+                return true;
+            } catch(err) {
+                console.log("Error checking if user exists: ", err.message);
+                return false;
             }
-
-            return true;
-        } catch(err) {
-            console.log("Error checking if user exists: ", err.message);
-            return false;
         }
     }
 })
